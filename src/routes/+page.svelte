7d5a9001421e5068902c327f6c1d1e972ecd5dc7
Antoni Siekański <catus.pl@gmail.com>
2023-05-08 13:54:41 +0200

    HTML layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,32 @@
-0000 0000 0000 0000 Jane Appleseed 00/00 000 Cardholder Name e.g. Jane Appleseed Card Number e.g.
-1234 5678 9123 0000 Exp. Date (MM/YY) MM YY CVC e.g. 123 Confirm
+<aside>
+	<div class="card">000</div>
+	<div class="card">0000 0000 0000 0000 Jane Appleseed 00/00</div>
+</aside>
 
-<!-- Completed state start -->
+<main>
+	<form>
+		<label for="name">Cardholder Name</label>
+		<input type="text" id="name" name="name" placeholder="e.g. Jane Appleseed" />
 
-Thank you! We've added your card details Continue
+		<label for="cardNumber">Card Number</label>
+		<input type="number" id="cardNumber" name="cardNumber" placeholder="e.g. 1234 5678 9123 0000" />
+
+		<label for="expDate">Exp. Date (MM/YY)</label>
+		<input type="number" id="expDate" name="expDateMonth" placeholder="MM" />
+		<input type="number" id="expDate" name="expDateYear" placeholder="YY" />
+
+		<label for="cvc">CVC</label>
+		<input type="number" id="cvc" name="cvc" placeholder="e.g. 123" />
+
+		<button>Confirm</button>
+	</form>
+
+	<!-- Completed state -->
+
+	{#if false}
+		Thank you! We've added your card details Continue
+	{/if}
+</main>
 
 <footer>
 	Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
