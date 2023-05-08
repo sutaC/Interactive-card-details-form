4a1fb687c05455ff0f6048ab8f8431a71b7fe26d
Antoni Siekański <catus.pl@gmail.com>
2023-05-08 13:43:17 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+0000 0000 0000 0000 Jane Appleseed 00/00 000 Cardholder Name e.g. Jane Appleseed Card Number e.g.
+1234 5678 9123 0000 Exp. Date (MM/YY) MM YY CVC e.g. 123 Confirm
+
+<!-- Completed state start -->
+
+Thank you! We've added your card details Continue
+
+<footer>
+	Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+		>Frontend Mentor</a
+	>. Coded by <a target="_blank" href="https://www.frontendmentor.io/profile/sutaC">sutaC</a>.
+</footer>
+
+<style>
+	footer {
+		width: 100%;
+		position: fixed;
+		left: 0;
+		bottom: 0;
+		text-align: center;
+		font-size: 10px;
+		mix-blend-mode: difference;
+	}
+</style>
